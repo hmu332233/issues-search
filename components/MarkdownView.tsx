ea3fa554b1bf64638f4e5a487f0b3ef997f9c2d4
Minung Han <hmu332233@gmail.com>
2022-04-16 18:07:59 +0900

    MarkdownView 추가

---
+++ components/MarkdownView.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import tomorrow from 'react-syntax-highlighter/dist/cjs/styles/prism/tomorrow';
+type Props = {
+  contents: string;
+};
+
+function MarkdownView({ contents }: Props) {
+  return (
+    <article className="prose">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            if (inline) {
+              return (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            }
+
+            const [, language] = /language-(\w+)/.exec(className || '') || [];
+            return (
+              <SyntaxHighlighter
+                style={tomorrow}
+                language={language || 'text'}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            );
+          },
+        }}
+      >
+        {contents}
+      </ReactMarkdown>
+    </article>
+  );
+}
+
+export default MarkdownView;
