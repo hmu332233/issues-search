5f3137f5336f2d16e792e97fffcb336901c60bf5
Minung Han <hmu332233@gmail.com>
2022-04-28 23:44:33 +0900

    site modal 사이즈 증가

--- packages/site/pages/index.tsx
+++ packages/site/pages/index.tsx
@@ -1,24 +1,27 @@
-import { useEffect, useState } from "react";
-import type { NextPage } from "next";
-import algoliasearch from "algoliasearch/lite";
+import { useEffect, useState } from 'react';
+import type { NextPage } from 'next';
+import algoliasearch from 'algoliasearch/lite';
 
-import classNames from "classnames";
+import classNames from 'classnames';
 
-import MarkdownView from "components/MarkdownView";
-import { normalizeData } from "utils";
+import MarkdownView from 'components/MarkdownView';
+import { normalizeData } from 'utils';
 
 import dummyData from 'utils/dummy';
-import PreviewMenu from "components/PreviewMenu";
+import PreviewMenu from 'components/PreviewMenu';
 
 const client = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_ID as string,
-  process.env.NEXT_PUBLIC_ALGOLIA_KEY as string
+  process.env.NEXT_PUBLIC_ALGOLIA_KEY as string,
 );
 const index = client.initIndex(process.env.NEXT_PUBLIC_ALGOLIA_INDEX as string);
 
 const Home: NextPage = () => {
   const [keyword, setKeyword] = useState('');
-  const [items, setItems] = useState<NormalizedData<SearchItem>>({ ids: [], entities: {} });
+  const [items, setItems] = useState<NormalizedData<SearchItem>>({
+    ids: [],
+    entities: {},
+  });
 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -35,8 +38,8 @@ const Home: NextPage = () => {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="modal modal-open py-10">
-        <div className="modal-box flex flex-col w-full max-w-4xl h-full max-h-832">
+      <div className="modal modal-open p-10">
+        <div className="modal-box flex flex-col w-full max-w-5xl h-full max-h-832">
           <form className="w-full" onSubmit={handleSearch}>
             <input
               name="query"
